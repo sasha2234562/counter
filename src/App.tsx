2adf35807e727d08f1c51cb96baf4cbdfbaa495e
Sasha <skalitsenia@mail.ru>
2023-07-18 09:39:05 +0300

    create counter

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      dfsdfs
+      <div>
+          <div></div>
+          <div></div>
+      </div>
     </div>
   );
 }
